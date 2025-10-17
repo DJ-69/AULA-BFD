fc3f9b5411087013f59face68e1f22ecfe69432d
DJ-69 <danieljesusscosta@gmail.com>
2025-10-17 15:30:37 -0300

    Delete Daniel Jesus/AULA-BFD/quarto.ts

--- Daniel Jesus/AULA-BFD/quarto.ts
+++
@@ -1,9 +0,0 @@
-async function Dadosip(id:number):Promise<any> {
-    const resposta =await fetch(`https://meowfacts.herokuapp.com/${id}`);
-    if(!resposta.ok) {
-        throw new Error("nao encontrei esse dados no IP");
-        
-    }
-    return resposta.json();
-}
-Dadosip(3);
