c4e469d201baf3033898c35dce8e9b0701c70a54
DJ-69 <danieljesusscosta@gmail.com>
2025-10-24 17:14:06 -0300

    Add files via upload

---
+++ Daniel Jesus/AULA-BFD/java script/Compras.js
@@ -0,0 +1,35 @@
+const entrada = require("./entrada");
+
+
+
+//o construdor determina o que cada class representa
+class Compra extends entrada {
+    constructor(livro, preco, vendedor, data_venda, qtd) {
+        this.livro = livro;
+        this.preco = preco;
+        this.vendedor = vendedor;
+        this.data_venda = data_venda;
+        this.qtd = qtd;
+        this.precoUnitario = this.calcularPrecoUnitario();
+        this.livro.qtdEstoque += qtd;
+    }
+    //a funçao CalcularPreçoUnitario serve para retorna um valor 
+
+    calcularPrecoUnitario() {
+        return this.preco / this.qtd;
+    }
+    //imprimi cada class do contrutor
+    imprimirDetalhes() {
+        console.log("---------------------");
+        console.log(`Livro: ${this.livro.titulo}`);
+        console.log(`Preço Total: R$ ${this.preco.toFixed(2)}`);
+        console.log(`Vendedor: ${this.vendedor}`);
+        console.log(`Data da Venda: ${this.data_venda}`);
+        console.log(`Quantidade: ${this.qtd}`);
+        console.log(`Preço Unitário: R$ ${this.precoUnitario.toFixed(2)}`);
+        console.log("---------------------");
+        console.log("gretrjtrjtythrejyt")
+    }
+}
+
+module.exports = Compra;
